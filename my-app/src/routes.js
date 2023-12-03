9efa5f6d441d1ac9021d31c84b287af2f7d3c788
Thais Oliveira <oliveirath27@gmail.com>
2023-12-03 00:55:33 -0300

    Heard e menu pronto, icons adicionados

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Ionicons, MaterialCommunityIcons,Feather} from '@expo/vector-icons';
+import { Image, TouchableOpacity, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
@@ -6,13 +8,91 @@ const Tab = createDrawerNavigator();
 
 import Index from './screens/Index';
 import Explorar from './screens/Explorar';
+import Listas from './screens/lista/Listas'
 export default function Rotas() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Index" component={Index} />
-        <Tab.Screen name="Explorar" component={Explorar} />
+      <Tab.Navigator  
+      screenOptions={{
+        drawerPosition:'right',
+        drawerLabel:'bold',
+        
+    }} >
+        <Tab.Screen name="Index" component={Index} 
+            options={({ navigation }) => ({
+                headerTitle: () => (
+                  <Image source={require('../assets/logo.png')}/>
+                ),
+                drawerLabel: ({ colort }) => <Text style={{ color:"#212529", fontWeight:  'bold' }}>
+                    { 'INDEX' }</Text>,
+                headerRight: () => (
+                  <TouchableOpacity
+                    style={{ marginRight: 10 }}
+                    onPress={() => navigation.openDrawer()}
+                  >
+                    <MaterialCommunityIcons name="menu-open" size={35} color="black"
+                    />
+                  </TouchableOpacity>
+                ),
+                headerLeft: () => null,
+
+                drawerIcon: ({ color, size }) => (
+                    <Ionicons name={'home-outline' } size={25} color={'black'} />
+                  ),
+                
+              })}
+        />
+        <Tab.Screen name="Explorar" component={Explorar} 
+            options={({ navigation }) => ({
+                headerTitle: () => (
+                  <Image source={require('../assets/logo.png')}/>
+                ),
+                drawerLabel: ({  color }) => <Text style={{ color:"#212529",fontWeight: 'bold'  }}>
+                {'EXPLORAR' }</Text>,
+                headerRight: () => (
+                  <TouchableOpacity
+                    style={{ marginRight: 10 }}
+                    onPress={() => navigation.openDrawer()}
+                  >
+                    <MaterialCommunityIcons name="menu-open" size={35} color="black"
+                    />
+                  </TouchableOpacity>
+                ),
+                headerLeft: () => null,
+
+                drawerIcon: ({ color, size }) => (
+                    <Ionicons name={'ios-search-outline' } size={25} color={'black'} />
+                  ),
+                
+              })}
+        />
+        <Tab.Screen name="Listas" component={Listas} 
+            options={({ navigation }) => ({
+                headerTitle: () => (
+                  <Image source={require('../assets/logo.png')}/>
+                ),
+                drawerLabel: ({  color }) => <Text style={{ color:"#212529",fontWeight: 'bold'  }}>
+                {'LISTAS' }</Text>,
+                headerRight: () => (
+                  <TouchableOpacity
+                    style={{ marginRight: 10 }}
+                    onPress={() => navigation.openDrawer()}
+                  >
+                    <MaterialCommunityIcons name="menu-open" size={35} color="black"
+                    />
+                  </TouchableOpacity>
+                ),
+                headerLeft: () => null,
+
+                drawerIcon: ({ color, size }) => (
+                    <Feather name={'list' } size={25} color={'black'} />
+                  ),
+                
+              })}
+        />
+        
+        
       </Tab.Navigator>
     </NavigationContainer>
   );
--- my-app/src/screens/lista/Listas.js
+++ my-app/src/screens/lista/Listas.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function Index() {
+export default function Listas() {
   return (
     <View >
-      <Text>Bem - Vindo! </Text>
+      <Text>Explorar</Text>
     </View>
   );
 }
